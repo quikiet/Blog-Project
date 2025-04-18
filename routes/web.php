ee2bbb911404cf39f01ad6d17c2353db8a761eb3
quikiet <quikiet03@gmail.com>
2025-04-18 13:34:36 +0700

    Add GoogleController

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\GoogleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/auth/google/redirect', [GoogleController::class, 'handleGoogleRedirect']);
+
+Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+
