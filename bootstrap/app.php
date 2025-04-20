a734c05c6adadb33c069e608d587e14f2741a910
quikiet <quikiet03@gmail.com>
2025-04-20 12:57:32 +0700

    Hosting 1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -19,7 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
-
+        $middleware->validateCsrfTokens(except: [
+            'api/*'
+        ]);
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
